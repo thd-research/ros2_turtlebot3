96e0b6ccf310757a440eb6b2669e63035263a030
dimadobriy <dobriy06@gmail.com>
2025-09-11 11:17:20 +0200

    add option for colcon build

--- 2_build_ws.sh
+++ 2_build_ws.sh
@@ -5,6 +5,6 @@ cd ~/turtlebot3_ws/src/turtlebot3
 rm -r turtlebot3_cartographer turtlebot3_navigation2
 cd ~/turtlebot3_ws/echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
 source ~/.bashrc
-colcon build --symlink-install --parallel-workers 1
+colcon build --symlink-install --parallel-workers 1 --executor sequential
 echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
 source ~/.bashrc
