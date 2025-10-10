eecfadaa43937da2c55167364af66207b3818901
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2025-10-10 12:52:21 +0200

    add controller forwarding

---
+++ ws_slam/wireless_controller.py
@@ -0,0 +1,44 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+
+class ControllerSubcriber(Node):
+    def __init__(self, node_name='controller_forward_subcriber'):
+        super().__init__(node_name)
+        
+        self.subscription = self.create_subscription(
+            Joy,
+            'joy',
+            self.listener_callback,
+            10
+        )
+
+        self.publisher = self.create_publisher(
+            Twist,
+            'cmd_vel',
+            0
+        )
+
+        self.linear_vel_scale = 0.22
+        self.angular_vel_scale = 2.8
+            
+    def listener_callback(self, msg):
+        self.get_logger().info(f"\rI heard a msg: {msg}")
+
+        vel = Twist()
+        vel.linear.x = msg.axes[1] * self.linear_vel_scale
+        vel.angular.z = msg.axes[0] * self.angular_vel_scale
+
+        self.publisher.publish(vel)
+        
+
+if __name__ == '__main__':
+    rclpy.init()
+
+    executor = rclpy.executors.SingleThreadedExecutor()
+
+    laser_subcriber = ControllerSubcriber(capacity=10)
+    rclpy.spin(laser_subcriber, executor=executor)
